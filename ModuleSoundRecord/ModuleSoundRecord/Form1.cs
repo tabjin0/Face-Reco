2b4d78d764e722ff7e49bd2b2aff42c6fa9d984c
jinboy <zjdefine@163.com>
2020-07-03 15:09:03 +0800

    ModuleSoundRecord

---
+++ ModuleSoundRecord/ModuleSoundRecord/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.DirectX.DirectSound;
+
+namespace ModuleSoundRecord
+{
+    public partial class Form1 : Form
+    {
+        private IAudioPlayer audioPlayer;
+        private IMicrophoneCapturer microphoneCapturer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
